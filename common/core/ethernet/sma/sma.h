892b9199811a154642f4d0b15cb1a315810224c3
Bex Saw <bex.bcc171@gmail.com>
2026-07-10 22:15:08 -0700

    read & write for ETH_MDIO

--- common/core/ethernet/sma/sma.h
+++ common/core/ethernet/sma/sma.h
@@ -6,11 +6,11 @@
 */
 
 #pragma once
-#include "st_eth_mdio.h"
-#include "eth_mdio.h"
-// #include "phy.h" 
+// #include "eth_mdio.h"
+// #include "st_eth_mdio.h"
+// #include "phy.h"
 
-// NOTE: Essentially, We going to have the interface where the SMA talking with the 
+// NOTE: Essentially, We going to have the interface where the SMA talking with the
 // PHY + RMII that is connected to the PHY (but we have a seperate driver for that)
 // NOTE: The SMA is going to be a wrapper for the MDIO driver, and the RMII driver is going to be a wrapper for the PHY driver
 namespace EoT
@@ -18,8 +18,6 @@ namespace EoT
 class SMA
 {
 public:
-
 private:
-
 };
-}
+}  // namespace EoT
