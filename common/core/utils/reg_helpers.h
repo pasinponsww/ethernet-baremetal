6a9b0a2c4f1dc9c2151edbc496a6e191ffd86c4f
Bex Saw <bex.bcc171@gmail.com>
2026-08-25 21:08:32 -0700

    test

--- common/core/utils/reg_helpers.h
+++ common/core/utils/reg_helpers.h
@@ -21,12 +21,7 @@ void SetReg(volatile uint32_t* reg, uint32_t enum_val, uint32_t bit_num,
 * @param bit_length Length of bit-field
 */
 template <typename T>
-void SetReg(T& reg, uint32_t value, uint32_t bit_num, uint32_t bit_length)
-{
-    const T mask = static_cast<T>((static_cast<T>(1U) << bit_length) - 1U);
-    reg &= static_cast<T>(~(mask << bit_num));
-    reg |= static_cast<T>((static_cast<T>(value) & mask) << bit_num);
-}
+void SetReg(T& reg, uint32_t value, uint32_t bit_num, uint32_t bit_length);
 
 /**
  * @brief Combines two bytes into a 16-bit value
